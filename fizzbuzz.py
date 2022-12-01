a0b8516f93f97ea0ced7ffad3b11e6bcac736e40
Jeanahi <117412366+Jeanahi@users.noreply.github.com>
2022-11-30 22:55:05 -0300

    ejercicio  de fizz buzz
    ejercicio cumpliendo con ciertas condiciones, puede ser modificado

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+for i in range(100):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif i % 3 == 0:
+        print("fizz")
+        continue
+    elif i % 5 == 0:
+        print("buzz")
+        continue
+    print(i)
+
